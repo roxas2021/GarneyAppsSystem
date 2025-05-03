2d9fc9741418ecb965d6f9c4d2c01fb17c6bab68
roxas2021 <roxasfredinz@gmail.com>
2025-05-03 17:05:58 +0800

    Add project files.

---
+++ GarneyAppSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GarneyAppSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
